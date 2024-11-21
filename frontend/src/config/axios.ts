9b97ddc8e1887d53d0e94c8267d287e03ac07763
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-21 17:53:43 -0500

    agregando interceptores para enviar el token si es qeu existe

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_URL_BACKEND,
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("auth_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
