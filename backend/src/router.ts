4ecdb95eeb665a31039dea5c5cd22a5a7809302e
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-20 23:35:02 -0500

    creando endpoint para regsitro de usuario y validando con express validator

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,5 +1,26 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { registerUser } from "./handlers/auth-handlers";
 
 const router = Router();
+
+router.post(
+  "/auth/register",
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre es requerido")
+    .isLength({ min: 2 })
+    .withMessage("El nombre debe tener mínimo 2 caracteres"),
+  body("handle").notEmpty().withMessage("El handle es requerido"),
+  body("email")
+    .notEmpty()
+    .withMessage("El email es requerido")
+    .isEmail()
+    .withMessage("Email inválido"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña debe tener mínimo 8 caracteres"),
+  registerUser
+);
 
 export default router;
