cc56500ae5751e690862a53002182d389be9a2b1
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-21 12:50:03 -0500

    usando la config de cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import router from "./router";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 
 const server = express();
+
+server.use(cors(corsConfig));
 
 server.use(express.json());
 server.use(router);
