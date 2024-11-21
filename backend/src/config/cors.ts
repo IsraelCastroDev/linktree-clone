78ddecb90da659c76df695773fa94dbacf771e4f
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-21 13:44:20 -0500

    permitiendo solicitudes thunder client

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -2,7 +2,13 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
-    if (origin === process.env.URL_FRONTEND) {
+    const whiteList = [process.env.URL_FRONTEND];
+
+    if (process.argv[2] === "--api") {
+      whiteList.push(undefined);
+    }
+
+    if (whiteList.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Errro de cors"));
