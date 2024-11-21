1fcbeaab8833ed8ee10543392bd268498e352bca
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-21 13:07:48 -0500

    vista de login con envio de datos

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,9 +1,88 @@
+import ErrorMessage from "@/components/ui/ErrorMessage";
+import { api } from "@/config/axios";
+import { LoginForm } from "@/types";
+import { isAxiosError } from "axios";
+import { CheckCircle, CircleAlertIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { toast } from "sonner";
 
 function LoginView() {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ defaultValues: initialValues });
+
+  const handleLogin = async (formData: LoginForm) => {
+    try {
+      const { data } = await api.post("/auth/login", formData);
+      toast(data.message, { icon: <CheckCircle className="h-5 w-5" /> });
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        toast(error.response.data.error, {
+          icon: <CircleAlertIcon className="w-5 h-5" />,
+        });
+      }
+    }
+  };
+
   return (
     <>
       <h1 className="text-4xl font-bold text-white">Iniciar sesión</h1>
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+        noValidate
+      >
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="email" className="text-2xl text-slate-500">
+            E-mail
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("email", {
+              required: "El Email es obligatorio",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "E-mail no válido",
+              },
+            })}
+          />
+          {errors.email && <ErrorMessage message={errors.email.message} />}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="password" className="text-2xl text-slate-500">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password de Registro"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("password", {
+              required: "El Password es obligatorio",
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage message={errors.password.message} />
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+          value="Iniciar Sesión"
+        />
+      </form>
       <nav className="mt-10">
         <Link
           to={"/auth/register"}
