8173061ac0b9a38003b3ca6678b5e7baf9354f59
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-22 00:45:24 -0500

    haciendo mas reutilizable el spinner

--- frontend/src/components/ui/Spinner.tsx
+++ frontend/src/components/ui/Spinner.tsx
@@ -1,9 +1,27 @@
 import styles from "./Spinner.module.css";
 
-export default function Spinner() {
+interface SpinnerProps {
+  message?: string;
+  color?: "slate" | "white";
+}
+
+export default function Spinner({ message, color = "slate" }: SpinnerProps) {
   return (
-    <div className="absolute top-1/2 left-1/2">
-      <span className={styles.loader}></span>
+    <div className="absolute top-64 left-1/2 flex justify-center items-center flex-col gap-2">
+      <span
+        className={`${styles.loader} border-4 ${
+          color === "white" ? "border-white" : "border-slate-800"
+        }`}
+      ></span>
+      {message && (
+        <p
+          className={`mt-3 font-semibold text-lg ${
+            color === "white" ? "text-white" : "text-slate-800"
+          } text-center`}
+        >
+          {message}
+        </p>
+      )}
     </div>
   );
 }
