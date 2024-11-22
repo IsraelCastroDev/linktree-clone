0fac3de6a8012ab89eb889f69b18a70549845fc8
IsraelCastroDev <israelcastro.dev@gmail.com>
2024-11-22 17:08:42 -0500

    guardando los datos en la bd y escribiendo la logica para agregar los links en ui

--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -1,13 +1,45 @@
+import { updateProfile } from "@/api/linktreeAPI";
 import SocialLinkInput from "@/components/SocialLinkInput";
 import { social } from "@/data/social";
-import { SocialLink } from "@/types";
+import { SocialLink, SocialNetwork, User } from "@/types";
 import { isValidUrl } from "@/utils";
-import { CircleAlertIcon } from "lucide-react";
-import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { CheckCircle, CircleAlertIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 export default function LinkTreeView() {
   const [socialLinks, setSocialLinks] = useState<SocialLink[]>(social);
+  const queryClient = useQueryClient();
+  const user: User = queryClient.getQueryData(["get-user"])!;
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: updateProfile,
+    onError(error) {
+      toast(error.message, {
+        icon: <CircleAlertIcon className="h-6 w-6" />,
+      });
+    },
+    onSuccess() {
+      toast("Enlace agregado correctamente", {
+        icon: <CheckCircle className="h-6 w-6" />,
+      });
+    },
+  });
+
+  useEffect(() => {
+    const updatedData = socialLinks.map((item) => {
+      const userLink = JSON.parse(user.links).find(
+        (link: SocialNetwork) => link.name === item.name
+      );
+      if (userLink) {
+        return { ...item, url: userLink.url, enabled: userLink.enabled };
+      }
+      return item;
+    });
+
+    setSocialLinks(updatedData);
+  }, []);
 
   const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const updatedLinks = socialLinks.map((link) =>
@@ -15,6 +47,8 @@ export default function LinkTreeView() {
     );
     setSocialLinks(updatedLinks);
   };
+
+  const links: SocialNetwork[] = JSON.parse(user.links);
 
   const handleEnableLink = (socialNetwork: string) => {
     const updatedLinks = socialLinks.map((link) => {
@@ -27,6 +61,57 @@ export default function LinkTreeView() {
       return link;
     });
     setSocialLinks(updatedLinks);
+
+    let updatedItems: SocialNetwork[] = [];
+
+    const selectedSocialNetwork = updatedLinks.find(
+      (link) => link.name === socialNetwork
+    );
+
+    if (selectedSocialNetwork?.enabled) {
+      const id = links.filter((link) => link.id).length + 1;
+
+      if (links.some((link) => link.name === socialNetwork)) {
+        updatedItems = links.map((link) => {
+          if (link.name === socialNetwork) {
+            return {
+              ...link,
+              enabled: true,
+              id,
+            };
+          } else {
+            return link;
+          }
+        });
+      } else {
+        const newItem = { ...selectedSocialNetwork, id };
+        updatedItems = [...links, newItem];
+      }
+    } else {
+      const indexToUpdate = links.findIndex(
+        (link) => link.name === socialNetwork
+      );
+      updatedItems = links.map((link) => {
+        if (link.name === socialNetwork) {
+          return {
+            ...link,
+            id: 0,
+            enabled: false,
+          };
+        } else if (link.id > indexToUpdate) {
+          return {
+            ...link,
+            id: link.id - 1,
+          };
+        } else {
+          return link;
+        }
+      });
+    }
+
+    queryClient.setQueryData(["get-user"], (prevData: User) => {
+      return { ...prevData, links: JSON.stringify(updatedItems) };
+    });
   };
 
   return (
@@ -39,6 +124,13 @@ export default function LinkTreeView() {
           handleEnableLink={handleEnableLink}
         />
       ))}
+      <button
+        onClick={() => mutate(user)}
+        className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-800 font-bold relative disabled:opacity-45 disabled:cursor-default"
+        disabled={isPending}
+      >
+        {isPending ? "Guardando cambios..." : "Guardar cambios"}
+      </button>
     </div>
   );
 }
